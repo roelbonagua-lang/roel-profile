3ce6abb3d8fec08c24a1ca0214b3ff58ab66c8c6
roelbonagua-lang <roelbonagua@gmail.com>
2026-03-17 19:09:15 +0800

    roel

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -172,24 +172,7 @@ export default function HeroSection() {
                                 },
                                 item: transitionVariants.item,
                             }}>
-                            <div className="mask-b-from-55% relative -mr-56 mt-8 overflow-hidden px-2 sm:mr-0 sm:mt-12 md:mt-20">
-                                <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
-                                    <Image
-                                        className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                                        src="/mail2.webp"
-                                        alt="app screen"
-                                        width="2700"
-                                        height="1440"
-                                    />
-                                    <Image
-                                        className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                        src="/mail2-light.webp"
-                                        alt="app screen"
-                                        width="2700"
-                                        height="1440"
-                                    />
-                                </div>
-                            </div>
+                           
                         </AnimatedGroup>
                     </div>
                 </section>
